c11bf8c87c78ba52983e3b9e4d9ad6d4eb32568b
lbq <1091043501@qq.com>
2019-09-21 15:35:35 +0800

    add_tech

---
+++ 技术/引用计数.cpp
@@ -0,0 +1,144 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+
+template<class T>
+class RCPtr
+{
+public:
+	RCPtr(T * realPtr = 0):pointee(realPtr)
+	{
+		init();
+	}
+	RCPtr(const RCPtr& rhs):pointee(rhs.pointee)
+	{
+		init();
+	}
+	~RCPtr()
+	{
+		if (pointee)
+			pointee->removeReference();
+	}
+	RCPtr& operator= (const RCPtr& rhs) 
+	{
+		if (pointee != rhs.pointee)
+		{
+			if (pointee)
+				pointee->removeReference();
+			pointee = rhs.pointee;
+			init();
+		}
+		return *this;
+	}
+	T * operator->() const
+	{
+		return pointee;
+	}
+	T& operator*() const
+	{
+		return *pointee;
+	}
+private:
+	T * pointee;
+	void init()
+	{
+		if (pointee == 0) return;
+		if (pointee->isShareable() == false)
+		{
+			pointee = new T(*pointee);
+		}
+		pointee->addReference();
+	}
+};
+
+class RCObject
+{
+public:
+	void addReference() { ++refCount; };
+	void removeReference() { if (--refCount == 0) delete this; };
+	void markUnshareable() { shareable = false; };
+	bool isShareable() const
+	{
+		return shareable;
+	};
+	bool isShared() const
+	{
+		return refCount > 1;
+	};
+protected:
+	RCObject() :refCount(0), shareable(true)
+	{}
+	RCObject(const RCObject& rhs) :refCount(0), shareable(true)
+	{}
+	RCObject& operator= (const RCObject& rhs)
+	{
+		return *this;
+	}
+	virtual ~RCObject() {};
+private:
+	int refCount;
+	bool shareable;
+};
+
+class My_String
+{
+public:
+	My_String(const char * data): value(new StrValue(data))
+	{}
+	const char& operator[] (int index) const
+	{
+		return value->sdata[index];
+	}
+	char& operator[] (int index)
+	{
+		if (value->isShared())
+		{
+			value = new StrValue(value->sdata);
+		}
+		value->markUnshareable();
+		return value->sdata[index];
+	}
+	~My_String()
+	{
+	/*	if (--value->refValue == 0)
+			delete value;
+		value = NULL;*/
+	}
+private:
+	struct StrValue:public RCObject
+	{
+		char * sdata;
+		void init(const char * initValue)
+		{
+			sdata = new char[strlen(initValue) + 1];
+			strcpy(sdata, initValue);
+		}
+		StrValue(const char * InitValue) 
+		{
+			if (InitValue != NULL)
+			{
+				init(InitValue);
+			}
+		}
+		StrValue(const StrValue& rhs)
+		{
+			init(rhs.sdata);
+		}
+		~StrValue()
+		{
+			delete[] sdata;
+		}
+	};
+	RCPtr<StrValue> value;
+};
+
+int main()
+{
+	My_String a("abcd");
+	
+	system("pause");
+	return 0;
+}
+
---
+++ 技术/引用计数2.cpp
@@ -0,0 +1,148 @@
+﻿#include "pch.h"
+#include <iostream>
+
+
+class RCObject
+{
+public:
+	void addReference() { ++refCount; };
+	void removeReference() { if (--refCount == 0) delete this; };
+	void markUnshareable() { shareable = false; };
+	bool isShareable() const
+	{
+		return shareable;
+	};
+	bool isShared() const
+	{
+		return refCount > 1;
+	};
+protected:
+	RCObject() :refCount(0), shareable(true)
+	{}
+	RCObject(const RCObject& rhs) :refCount(0), shareable(true)
+	{}
+	RCObject& operator= (const RCObject& rhs)
+	{
+		return *this;
+	}
+	virtual ~RCObject() {};
+private:
+	int refCount;
+	bool shareable;
+};
+
+template<class T>
+class RCIPtr
+{
+private:
+	struct CountHolder :public RCObject
+	{
+		~CountHolder() { delete pointee; }
+		T *pointee;
+	};
+	CountHolder *counter;
+public:
+	RCIPtr(T * realPtr = 0) :counter(new CounterHolder)
+	{
+		counter->pointee = realPtr;
+		init();
+	}
+	RCIPtr(const RCIPtr& rhs):counter(rhs.counter)
+	{
+		init();
+	}
+	~RCIPtr()
+	{
+		counter->removeReference();
+	}
+	RCIPtr& operator= (const RCIPtr& rhs)
+	{
+		if (counter != rhs.counter)
+		{
+			counter->removeReference();
+			counter = rhs.counter;
+			init();
+		}
+		return *this;
+	}
+	const T* operator->() const
+	{
+		return counter->pointee;
+	}
+	T * operator->()
+	{
+		makeCopy();
+		return counter->pointee;
+	}
+	const T& operator* () const
+	{
+		return *(counter->pointee);
+	}
+	T& operator* ()
+	{
+		makeCopy();
+		return *(counter->pointee);
+	}
+private:
+	void init()
+	{
+		if (counter->isShareable() == false)
+		{
+			T * oldValue = counter->pointee;
+			counter = new CounterHolder;
+			counter->pointee = new T(*oldValue);
+		}
+		counter->addReference();
+	}
+	void makeCopy()
+	{
+		if (counter->isShared())
+		{
+			T * oldValue = counter->pointee;
+			counter->removeReference();
+			counter = new CounterHolder;
+			counter->pointee = new T(*oldValue);
+			cpunter->addReference();
+		}
+	}
+};
+
+class Widget
+{
+public:
+	Widget(int size);
+	Widget(const Widget& rhs);
+	~Widget();
+	Widget& operator= (const Widget& rhs);
+	void doThis();
+	int showThat() const;
+};
+
+class RCWidget
+{
+public:
+	RCWidget(int size):value(new Widget(size)){}
+	void doThis() { value->doThis(); }
+	int showThat() const
+	{
+		return value->showThat();
+	}
+private:
+	RCIPtr<Widget> value;
+};
+
+
+
+
+
+int main()
+{
+
+
+
+
+
+
+	getchar();
+	return 0;
+}
